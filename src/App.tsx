cffd17d6be64d0521d215b321aa7161140295084
Valeryia Visheuskaya <valeriavishevskaya3@gmail,com>
2023-06-14 21:34:01 +0200

    first step,layoat, css, grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="#" alt="avatar_my" />
       </header>
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="#" alt="background-color" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            new post
+          </div>
+          <div>
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+          <div>
+            post 3
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
