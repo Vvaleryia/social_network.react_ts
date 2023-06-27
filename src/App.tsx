3d2b96da9c462cd3504216dcc17f7f7327b398f1
Vvisheuskaya <vvisheuskaya@gmail.com>
2023-06-27 17:21:19 +0200

    fracturing of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  return (
-    <div className='app-wrapper'>
-      <header className='header'>
-        <img src="#" alt="avatar_my" />
-      </header>
-      <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src="#" alt="background-color" />
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            new post
-          </div>
-          <div>
-          </div>
-          <div>
-            post 1
-          </div>
-          <div>
-            post 2
-          </div>
-          <div>
-            post 3
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="#" alt="background-color"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                new post
+            </div>
+            <div>
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+            <div>
+                post 3
+            </div>
+        </div>
+    </div>
+};
+
+export default Profile;
